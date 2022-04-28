76fe08442997ce72e9faf629bbe721363282de37
Pedro Henrique Eifler <62551886+pedroEifler@users.noreply.github.com>
2022-04-27 22:08:06 -0300

    CoffeeBook - Base do Projeto

---
+++ src/test/java/br/com/coffeeBook/CoffeeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.coffeeBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
